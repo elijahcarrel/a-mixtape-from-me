3cee85d1fc61f6a9abfe4a122236a5aea2079792
Elijah Carrel <elijahcarrel@gmail.com>
2025-07-19 22:43:29 -0400

    check openapi spec into repo and add an action to enforce it's kept up to date

---
+++ scripts/generate_openapi_spec.py
@@ -0,0 +1,27 @@
+import argparse
+import json
+import sys
+import os
+from fastapi.openapi.utils import get_openapi
+
+# Add the project root to the path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from backend.app_factory import create_app
+
+app = create_app()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output', default='openapi.json', help='Output file for generated OpenAPI schema')
+args = parser.parse_args()
+
+openapi_spec = get_openapi(
+    title=app.title,
+    version=app.version,
+    openapi_version=app.openapi_version,
+    description=app.description,
+    routes=app.routes,
+)
+
+with open('openapi.json', 'w') as f:
+    json.dump(openapi_spec, f, indent=4)
