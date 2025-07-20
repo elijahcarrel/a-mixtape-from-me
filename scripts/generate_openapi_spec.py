80bfffaefe371b2f44051d134cd2b68e144ea5c7
Elijah Carrel <elijahcarrel@gmail.com>
2025-07-19 23:30:40 -0400

    switch to yaml?

--- scripts/generate_openapi_spec.py
+++ scripts/generate_openapi_spec.py
@@ -1,7 +1,7 @@
 import argparse
-import json
 import sys
 import os
+import yaml
 from fastapi.openapi.utils import get_openapi
 
 # Add the project root to the path
@@ -12,7 +12,7 @@ from backend.app_factory import create_app
 app = create_app()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--output', default='openapi.json', help='Output file for generated OpenAPI schema')
+parser.add_argument('--output', default='openapi.yaml', help='Output file for generated OpenAPI schema')
 args = parser.parse_args()
 
 openapi_spec = get_openapi(
@@ -24,4 +24,4 @@ openapi_spec = get_openapi(
 )
 
 with open(args.output, 'w') as f:
-    json.dump(openapi_spec, f, indent=4)
+    yaml.dump(openapi_spec, f)
