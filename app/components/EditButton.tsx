eeff48c137a03058c94a52bf9333ea99057e7467
Elijah Carrel <elijahcarrel@gmail.com>
2025-07-21 00:14:17 -0400

    add comment

--- app/components/EditButton.tsx
+++ app/components/EditButton.tsx
@@ -12,6 +12,8 @@ export default function EditButton({ mixtape }: EditButtonProps) {
   const { user } = useAuth({ requireAuth: false });
   const router = useRouter();
 
+  // Only show the "Edit" button on a mixtape if the user who owns
+  // the mixtape is viewing it.
   if (!user || user.id !== mixtape.stack_auth_user_id) {
     return null;
   }
