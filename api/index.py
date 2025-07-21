f72dd3569ba930c014a30feb13426af76c3ea9d9
Elijah Carrel <elijahcarrel@gmail.com>
2025-07-20 21:35:32 -0400

    idkkk

---
+++ api/index.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from backend.app_factory import create_app
+
+# Load environment variables from .env.local file in development.
+if os.getenv('VERCEL_ENV') is None:
+    load_dotenv('.env.local')
+
+# Create app with production database
+database_url = os.getenv('DATABASE_URL')
+app = create_app(database_url)
--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/main/:path*",
+        source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/main/:path*"
+            ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
       },
       {
