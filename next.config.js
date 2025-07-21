1a6511940bac3c3b78ad3a69d12ae1fff57db288
Elijah Carrel <elijahcarrel@gmail.com>
2025-07-20 21:26:21 -0400

    idk maybe this

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
             : "/api/main/:path*",
       },
       {
+        source: "/api/main/:path",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/main/:path"
+            : "/api/main/:path",
+      },
+      {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
