1f51b59f1afbf6d7b5ee3ced46ee4fc30f394190
Elijah Carrel <elijahcarrel@gmail.com>
2025-07-26 17:38:23 -0400

    skip build for dependabot PRs (#38)
    * skip build for dependabot PRs
    
    * add comment
    
    * some more

---
+++ scripts/vercel_ignore_build_step.sh
@@ -0,0 +1,12 @@
+# This script is called by Vercel to determine if the build should be skipped.
+# We configure this script to be called in the Vercel project settings.
+
+# It is used to skip the build for Dependabot PRs.
+
+if [ "$VERCEL_GIT_COMMIT_AUTHOR_LOGIN" == "dependabot[bot]" ]; then
+    echo "🛑 - Build cancelled for Dependabot PR"
+    exit 0; # Exit with 0 to skip the build
+else
+    echo "✅ - Build can proceed"
+    exit 1; # Exit with 1 to trigger the build
+fi
