801423d60e17fb321ca48ae180359a89c0b40c12
Elijah Carrel <elijahcarrel@gmail.com>
2025-07-26 19:05:37 -0400

    shrink user button on small screens

--- app/components/layout/HeaderContainer.tsx
+++ app/components/layout/HeaderContainer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HeaderContainer({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 sm:gap-0 mb-4 sm:mb-6 md:mb-8"
+      className="flex flex-row justify-between items-center gap-4 mb-4 sm:mb-6 md:mb-8"
     >
       {children}
     </div>
