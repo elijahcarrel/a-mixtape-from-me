b4ad929626031a5b260989da9329c53c0f9f30fa
Elijah Carrel <elijahcarrel@gmail.com>
2025-08-25 22:10:12 -0400

    delete unnecessary generated heyapi-ts code that we don't use (#79)
    these were generated function stubs from our openapi spec, but we wrote our own functions for this (i.e. `useAuthenticatedRequest`) in a different format and don't use the generated once. we can always add it back if we decide we want it

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
   input: 'openapi.gen.json',
-  output: 'app/client',
+  output: {
+    path: 'app/client',
+    format: 'prettier',
+  },
+  // We only use the typescript plugin for now.
+  // In the future we may consider using some auto-generated
+  // functions (such as tanstack-query) in lieu of the manual
+  // and untyped hooks we currently use.
+  plugins: ['@hey-api/typescript'],
 });
