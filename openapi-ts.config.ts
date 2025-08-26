fff15822d967d0b135214c045c7ce1694839b2f2
Elijah Carrel <elijahcarrel@gmail.com>
2025-08-25 22:05:56 -0400

    add comment explaining our decision

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
     path: 'app/client',
     format: 'prettier',
   },
+  // We only use the typescript plugin for now.
+  // In the future we may consider using some auto-generated
+  // functions (such as tanstack-query) in lieu of the manual
+  // and untyped hooks we currently use.
   plugins: ['@hey-api/typescript'],
 });
